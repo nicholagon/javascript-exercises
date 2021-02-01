ba99377c85f7a0997938a72f831283161aa2e590
_weirdatlast <nicholagon@gmail.com>
2021-02-02 00:08:12 +0800

    Add repeatString functionalities, including handling of negative cases

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function(inputString, count) {
+    let repeatedString = '';
 
+    if(count < 0) return 'ERROR';
+
+    for(let i = 0; i < count; i++) {
+        repeatedString += inputString;
+    }
+    
+    return repeatedString;
 }
 
 module.exports = repeatString
