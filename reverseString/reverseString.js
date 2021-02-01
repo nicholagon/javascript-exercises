0f8cb1d2b37f5ebf633802c5a72235a0c9507a94
_weirdatlast <nicholagon@gmail.com>
2021-02-02 03:31:41 +0800

    Added reverseString functionalities with passing cases

--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
+const reverseString = function(inputString) {
+    let splitStringArr = inputString.split('');
+    let reverseStringArr = [];
 
+    for(let i = 1; i <= splitStringArr.length; i++) {
+        reverseStringArr.push(splitStringArr[splitStringArr.length - i]);
+    }
+
+    let reversedString = reverseStringArr.join('');
+    return reversedString;
 }
 
 module.exports = reverseString
