8e390f868e4dbf4432da870ec7894b872d64f005
_weirdatlast <nicholagon@gmail.com>
2021-02-03 04:21:24 +0800

    Add removeFromArray funcitonality including negative cases

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,4 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(...theArgs) {
+    let argsArray = theArgs;
+    let referenceArray = argsArray.shift();
+
+    for(let i = 0; i <= argsArray.length - 1; i++) {
+        if(referenceArray.includes(argsArray[i])) {
+            referenceArray.splice(referenceArray.indexOf(argsArray[i]), 1);
+        }
+        else continue;
+    }
+
+    return referenceArray;
 
 }
 
