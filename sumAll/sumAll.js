4a21aae47cffed6fa852080d0ce8a6b7ae41e496
_weirdatlast <nicholagon@gmail.com>
2021-02-03 00:55:45 +0800

    Add sumAll functionality including handling of negative cases

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,4 +1,30 @@
-const sumAll = function() {
+const sumAll = function(firstInput, secondInput) {
+    let max = 0;
+    let min = 0;
+    let sum = 0;
+
+    if(!(Number.isInteger(firstInput)) || !(Number.isInteger(secondInput))) {
+        return "ERROR";
+    }
+
+    if(firstInput < 0 || secondInput < 0) {
+        return "ERROR";
+    }
+    
+    if(firstInput > secondInput) {
+        max = firstInput;
+        min = secondInput;
+    }
+    else if(firstInput < secondInput) {
+        max = secondInput;
+        min = firstInput;
+    }
+
+    for(let i = min; i <= max; i++) {
+        sum += i;
+    }
+
+    return sum;
 
 }
 
