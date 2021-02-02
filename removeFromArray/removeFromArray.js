35c75ff5f3c9769db49d10d0734c39e2a186d839
_weirdatlast <nicholagon@gmail.com>
2021-02-03 04:34:09 +0800

    Revert "Add removeFromArray funcitonality including negative cases"
    This reverts commit 8e390f868e4dbf4432da870ec7894b872d64f005.

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,15 +1,4 @@
-const removeFromArray = function(...theArgs) {
-    let argsArray = theArgs;
-    let referenceArray = argsArray.shift();
-
-    for(let i = 0; i <= argsArray.length - 1; i++) {
-        if(referenceArray.includes(argsArray[i])) {
-            referenceArray.splice(referenceArray.indexOf(argsArray[i]), 1);
-        }
-        else continue;
-    }
-
-    return referenceArray;
+const removeFromArray = function() {
 
 }
 
